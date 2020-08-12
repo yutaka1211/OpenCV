cceea690598263eaed53b5441b6122ab664d4bc9
yutaka1211 <star12wars11@hotmail.co.jp>
2020-08-12 17:21:05 +0900

    Create Cell_count

--- Cell_count/cell_count.py
+++ Cell_count/cell_count.py
@@ -14,24 +14,26 @@ out = distmap * 0                               #マップを初期化
 ksize = 10
 
 #周辺の20*20ピクセルの領域中で最も値が大きい場所を1とする
-for x in range(ksize, distmap.shape[0]-ksize*2):
-    for y in range(ksize, distmap.shape[1]-ksize*2):
+for x in range(ksize, distmap.shape[0]-ksize):
+    for y in range(ksize, distmap.shape[1]-ksize):
         if distmap[x, y] > 0 and distmap[x, y] == np.max(distmap[x-ksize:x+ksize, y-ksize:y+ksize]):
             out[x, y]=1
 
-out = cv2.dilate(out, (3,3))
+out = cv2.dilate(out, (3,3))                    #3*3のカーネルによって膨張させる->近くに同じ極大値をもつピクセルがあれば、1つの塊にする
 
+#変数(入力画像,抽出モード,近似手法)
 contours, _ = cv2.findContours(out.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+#contoursは検出された前輪郭をlistとして出力,list内の各輪郭は輪郭上の点の(x,y)座標をnumpyのarrayとして保存
 
-arr = []
+arr = []                                        #[]で配列を作ると早い
 for i in contours:
     x_ = 0
     y_ = 0
     for j in i:
         x_ += j[0][0]
         y_ += j[0][1]
-    arr.append([x_/len(i), y_/len(i)])
-arr = np.array(arr)
+    arr.append([x_/len(i), y_/len(i)])          #arrの末尾に要素を追加
+arr = np.array(arr)                             #リストをnumpy配列に変換
 
 plt.imshow(out)
 plt.colorbar()
@@ -39,3 +41,6 @@ plt.show()
 
 print(len(arr))
 print(arr)
+
+#dilateはカーネル内に画素値が1の画素が1つでもあれば、出力画像の注目画素の画素値を1にする
+#cv2.CHAIN_APPROX_SIMPLE:findcontoursの近似手法
